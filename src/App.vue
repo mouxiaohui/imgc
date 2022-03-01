addc6777342f526927f82e0db5fafd302407f8dc
xiao hui <80144167+mouxiaohui@users.noreply.github.com>
2022-03-01 22:56:09 +0800

    upload image

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div class="upload">
+    <label>
+      <input class="hidden" type="file" />
+      <div>上传图片</div>
+      <img src="./assets/image/upload.svg" />
+    </label>
+  </div>
+</template>
 
-<style></style>
+<style lang="scss">
+.upload {
+  @apply flex w-28 border border-white justify-center;
+
+  img {
+    @apply w-10 pl-3;
+  }
+}
+</style>
